cd5ceeb926cdaa1b90b93cedda4c4f4e5dfb3a7b
Bohdan Hamulets <bogdan.robota@gmail.com>
2017-12-11 12:11:04 +0200

    Update diagnostik.sh

--- diagnostik.sh
+++ diagnostik.sh
@@ -1,5 +1,5 @@
 #!/bin/csh
 
 while ( 1 ) 
-	date >> hermes_every_10min.log && top |grep hermes >> hermes_every_10min.log && echo "" >> hermes_every_10min.log && sleep 10
+	date >> hermes_every_10min.log && top |grep hermes >> hermes_every_10min.log && echo "" >> hermes_every_10min.log && sleep 600
 end
