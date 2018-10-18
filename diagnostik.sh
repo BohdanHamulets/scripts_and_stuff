c33f9ecb567de33ee4d88a4871b416990565bc74
Bohdan Hamulets <bogdan.robota@gmail.com>
2018-10-18 17:33:16 +0300

    Update diagnostik.sh

--- diagnostik.sh
+++ diagnostik.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-while ( 1 ) 
-	date >> hermes_every_10min.log && top |grep hermes >> hermes_every_10min.log && echo "" >> hermes_every_10min.log && sleep 600
-end
+while true
+do
+    echo `date` >> hermes_every_10min.log && top |grep hermes >> hermes_every_10min.log && echo "" >> hermes_every_10min.log
+    sleep 2
+done
